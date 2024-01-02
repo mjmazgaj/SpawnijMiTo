87b5bfe84a8f5f2ff1a49f33a384dae96cc25997
Mazgaj <>
2024-01-02 23:36:05 +0100

    Display all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import Nav from "./components/nav/Nav";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Nav />
+      <Header />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
