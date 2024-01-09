a096f036bed4af5b4f74808b6cf9dafb7dece065
Mazgaj <>
2024-01-09 19:59:55 +0100

    Portfolio : Content | Change wording, image size and add new project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import "./portfolio.min.css";
 import IMG1 from "../../assets/portfolio/portfolio1.jpg";
 import IMG2 from "../../assets/portfolio/portfolio2.jpg";
 import IMG3 from "../../assets/portfolio/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio/portfolio4.jpg";
 import IMG9 from "../../assets/portfolio/portfolio9.jpg";
 import IMG10 from "../../assets/portfolio/portfolio10.jpg";
 import IMG20 from "../../assets/portfolio/portfolio20.jpg";
@@ -10,18 +11,24 @@ const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Stolik Hi-fi długi",
+    title: "Stolik Hi-fi",
     description: "Solidna stalowa konstrukcja wykonana z profili stalowych 40x40 i 40x30, pomalowana proszkowo na matowy czarny kolor o drobnej strukturze. Blaty z dębu o grubości 30 mm, wyposażone w niskie kolce oraz podkładki w kolorze złota."
   },
   {
     id: 2,
     image: IMG2,
-    title: "Stolik Hi-fi krótki",
-    description: "Wykonany z malowanej proszkowo stali oraz blatów dębowych w naturalnym kolorze. Całość uzupełniona o czarne kolce i czarne podkładki."
+    title: "Stolik Rtv XXL",
+    description: "Blaty wykonane z drewna dębowego, olejowane na kolor bezbarwny. Konstrukcja malowana proszkowo, kolor czarny mat o drobnej strukturze. Kolce koloru czarnego z czarnymi podkładkami."
   },
   {
     id: 3,
     image: IMG3,
+    title: "Stolik Hi-fi krótki",
+    description: "Wykonany z malowanej proszkowo stali oraz blatów dębowych w naturalnym kolorze. Całość uzupełniona o czarne kolce i czarne podkładki."
+  },
+  {
+    id: 4,
+    image: IMG4,
     title: "Regał pod sprzęt audio",
     description: "Wykonane z drewna dębowego blaty, delikatnie olejowane na bezbarwny odcień. Konstrukcja malowana proszkowo na matowy czarny kolor o drobnej strukturze. Kolce w kolorze czarnym z czarnymi podkładkami."
   },
